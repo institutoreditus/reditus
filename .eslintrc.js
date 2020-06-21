40ed11c9eddf19fce23fcf5ae4d1f60deb3cf918
Bernardo Amorim <contato@bamorim.com>
2020-06-21 19:27:29 -0300

    chore: create CI workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,15 @@ module.exports = {
     'browser': true,
     'es2020': true,
     'node': true,
+    "jest/globals": true,
   },
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
     'google',
-    'prettier'
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:jest/style'
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
@@ -19,15 +22,17 @@ module.exports = {
     'sourceType': 'module',
   },
   'plugins': [
+    'jest',
     'react',
     '@typescript-eslint',
-    'prettier'
+    'prettier',
   ],
   'rules': {
     "react/react-in-jsx-scope": "off",
     "require-jsdoc": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "jest/no-export": "off"
   },
 };
---
+++ __tests__/hello.test.ts
@@ -0,0 +1,9 @@
+// This directory should contain all e2e tests, but for now we are only adding a
+// example test to get us started
+
+test("one plus one is two", () => {
+  expect(2 + 2).toBe(4);
+});
+
+// TODO: Discover how to make TSC ignore modules when talking about test files.
+export {};
