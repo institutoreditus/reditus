0bb7052fc6927a7f10fca457bfbde2489bdc12da
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-07-25 13:40:41 -0300

    fix: adjust responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
       <main>
         <GridRow>
-          <GridCell className={styles.leftSide} span={4} align={"middle"}>
+          <GridCell order={0} className={styles.leftSide} desktop={4} align={"middle"}>
             <p>Tornando-se parte dessa iniciativa você...</p>
             <List twoLine>
               <SimpleListItem
@@ -42,7 +42,7 @@ export default function Home() {
               />
             </List>
           </GridCell>
-          <GridCell className={styles.rightSide} span={8} align={"middle"}>
+          <GridCell order={1} className={styles.rightSide} desktop={8} tablet={12} phone={12} align={"middle"}>
             <img src="./logoReditusWhite.png" />
             <p className="title">
               Faça parte dessa corrente do bem! Ajude a fomentar uma cultura de
