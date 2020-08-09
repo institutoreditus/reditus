df4c088cdb54860db6636653f3a6e8510684abcb
rrozendo <41521484+rrozendo@users.noreply.github.com>
2020-08-09 20:05:59 -0300

    feat: front-end for the online donation system (#22)
    Adding front-end for the online donation system

---
+++ components/InputDonationValues.tsx
@@ -0,0 +1,159 @@
+import { NavigationButtons } from "./action_navigate/NavigationButtons";
+import NumberFormat from "react-number-format";
+import { TextField } from "@rmwc/textfield";
+import { Button } from "@rmwc/button";
+import axios from "axios";
+
+import styles from "./Form.module.css";
+
+declare let PagarMeCheckout: any;
+const encryptionKey = process.env.PAGARME_ENC_KEY;
+let checkedRadio: any;
+
+export const InputDonationValues = (props: any) => {
+  const validate = () => {
+    props.previousStep();
+  };
+
+  const successDonation = () => {
+    props.goToStep(3);
+  };
+
+  const failedDonation = () => {
+    props.goToStep(4);
+  };
+
+  const update = (e: any) => {
+    if (e.target && e.target.type === "radio") {
+      checkedRadio = e.target;
+    }
+    props.update(e.target.name, e.target.value);
+  };
+
+  async function onCheckout(e: any) {
+    e.preventDefault();
+
+    // TODO: Ask that somewhere in the frontend
+    const amountInCents = props.form.amountInCents * 100;
+
+    // Create checkout instance
+    const checkout = new PagarMeCheckout.Checkout({
+      encryption_key: encryptionKey,
+      success: async function (data: any) {
+        try {
+          await axios.post("/api/contributions", data);
+          return successDonation();
+        } catch (err) {
+          // alert("Erro ao doar");
+          return failedDonation();
+        }
+      },
+      error: function () {
+        // alert("Erro ao doar");
+        return failedDonation();
+      },
+      close: function () {
+        console.log("The modal has been closed.");
+      },
+    });
+
+    // Checkout API require booleans to be passed as strings
+    checkout.open({
+      amount: amountInCents,
+      buttonText: "Contribuir",
+      buttonClass: "botao-pagamento",
+      customerData: "true",
+      createToken: "false",
+      paymentMethods: "credit_card",
+      uiColor: "#00d4ff",
+      headerText: "Vou contribuir com {price_info}",
+    });
+  }
+
+  return (
+    <div>
+      {props.form.firstname && <h3>Hey {props.form.teste}!</h3>}
+      <NavigationButtons step={2} {...props} previousStep={validate} />
+
+      <div className={styles.donationValues}>
+        <div className={styles.defaultValues}>
+          <input
+            className={styles.defaultValues__value}
+            type="radio"
+            value={25}
+            name="amountInCents"
+            onChange={update}
+            id="firstDefaultValue"
+          />
+          <label
+            className={styles.defaultValues__value}
+            htmlFor="firstDefaultValue"
+          >
+            R$ 25
+          </label>
+
+          <input
+            className={styles.defaultValues__value}
+            type="radio"
+            value={50}
+            name="amountInCents"
+            onChange={update}
+            id="secondDefaultValue"
+          />
+          <label
+            className={styles.defaultValues__value}
+            htmlFor="secondDefaultValue"
+          >
+            R$ 50
+          </label>
+
+          <input
+            className={styles.defaultValues__value}
+            type="radio"
+            value={75}
+            name="amountInCents"
+            onChange={update}
+            id="thirdDefaultValue"
+          />
+          <label
+            className={styles.defaultValues__value}
+            htmlFor="thirdDefaultValue"
+          >
+            R$ 75
+          </label>
+        </div>
+        <p>Vou doar: R$ {props.form.amountInCents}</p>
+        <div id={styles.customValue}>
+          <NumberFormat
+            label="Quero doar outro valor..."
+            prefix={"R$"}
+            id={styles.customValue__input}
+            name="amountInCents"
+            customInput={TextField}
+            thousandSeparator={true}
+            allowNegative={false}
+            onValueChange={(values) => {
+              if (checkedRadio) {
+                checkedRadio.checked = false;
+              }
+
+              const value = values.value;
+
+              props.update("amountInCents", value);
+            }}
+            fullwidth
+          />
+        </div>
+      </div>
+      <Button
+        label="Doar agora"
+        raised
+        unelevated
+        onClick={onCheckout}
+        id={styles.defaultButton}
+      />
+    </div>
+  );
+};
+
+export default InputDonationValues;
---
+++ components/SuccessDonation.tsx
@@ -0,0 +1,13 @@
+export const SuccessDonation = () => {
+  return (
+    <div>
+      <h1>Doação concluída com sucesso!</h1>
+      <p>
+        Agradecemos por escolher fazer parte dessa iniciativa. Te enviaremos
+        também um email de confirmação da sua doação.
+      </p>
+    </div>
+  );
+};
+
+export default SuccessDonation;
