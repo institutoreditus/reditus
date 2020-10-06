7a9c22ee51787fd30d1f7610a7093c7a1d4bd839
Jonatas Teixeira <jonatasteixeira@localhost.localdomain>
2020-10-06 20:33:51 -0300

    feat: adding actions to state management

---
+++ src/Stepper/actions/donationFormData.tsx
@@ -0,0 +1,10 @@
+export default function updateAction(state: any, payload: any) {
+    return {
+      ...state,
+      donationFormData: {
+        ...state.donationFormData,
+        ...payload,
+      },
+    };
+  }
+  
