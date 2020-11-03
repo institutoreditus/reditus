1f7868538467da8bd38b6a4cb05bb7648f741559
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2020-11-02 21:29:46 -0300

    chore: updating form style with rmwc
    Signed-off-by: Jonatas Teixeira <jonatasteixeira@id.uff.br>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@material/theme/dist/mdc.theme.css";
 import "@rmwc/theme/theme.css";
+import "@material/form-field/dist/mdc.form-field.css";
+import "@material/typography/dist/mdc.typography.css";
 import "@material/button/dist/mdc.button.css";
+import "@material/checkbox/dist/mdc.checkbox.css";
 import "@material/layout-grid/dist/mdc.layout-grid.css";
 import "@material/textfield/dist/mdc.textfield.css";
 import "@material/list/dist/mdc.list.css";
@@ -8,6 +11,7 @@ import "@material/ripple/dist/mdc.ripple.css";
 import "@rmwc/icon/icon.css";
 import "@material/icon-button/dist/mdc.icon-button.css";
 import "@material/radio/dist/mdc.radio.css";
+import "@material/drawer/dist/mdc.drawer.css";
 
 import "../styles.css";
 
