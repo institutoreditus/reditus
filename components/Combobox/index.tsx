7f8e6c5f355e0fb6597c83f9d641c5cd6adef652
Jonatas Teixeira <jonatasteixeira@id.uff.br>
2021-01-11 18:38:46 -0300

    fix: run lint

--- components/Combobox/index.tsx
+++ components/Combobox/index.tsx
@@ -3,33 +3,37 @@ import { Select } from "@rmwc/select";
 function Options({ options }: any) {
   return options.map((option: any) => (
     <option key={option.id} value={option.name}>
-      {!option.label? option.name: `(${option.name})`} {option.label}
+      {!option.label ? option.name : `(${option.name})`} {option.label}
     </option>
   ));
 }
 
-function YearList() {
+function yearList() {
   const year = new Date().getFullYear();
   const range = 89;
-  return (
-    Array.from( new Array(90), (_v,i) => 
-      <option key={i} value={year-range+i}>{year-range+i}</option>
-    )
-  );
-};
+  return Array.from(new Array(90), (_v, i) => (
+    <option key={i} value={year - range + i}>
+      {year - range + i}
+    </option>
+  ));
+}
 
 export const Combobox = (props: any): JSX.Element => {
   return (
-      <Select
-        label={props.label}
-        placeholder={props.placeholder}
-        name={props.name}
-        onChange={props.onChange}
-        type={props.type}
-      >
-        {props.type == "number"? YearList(): <Options width="50px" options={props.dataset}/>}
-        <option value="outros">{props.otherOption}</option>
-      </Select>
+    <Select
+      label={props.label}
+      placeholder={props.placeholder}
+      name={props.name}
+      onChange={props.onChange}
+      type={props.type}
+    >
+      {props.type == "number" ? (
+        yearList()
+      ) : (
+        <Options width="50px" options={props.dataset} />
+      )}
+      <option value="outros">{props.otherOption}</option>
+    </Select>
   );
 };
 
