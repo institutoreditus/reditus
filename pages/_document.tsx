1da06e56c00d9784bb87db7b5f5d9a3bd1c3d825
Jonatas Teixeira <jonatasteixeira@id.uff.br>
2021-02-07 09:46:27 -0500

    feat: Add support for Google Tag Manager
    Signed-off-by: Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 import { GA_TRACKING_ID } from "../helpers/gtag";
+import { GTM_TRACKING_ID } from "../helpers/gtm";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -9,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* enable analytics script only for production */}
+          {/* Enables analytics script only in production */}
           {isProduction && GA_TRACKING_ID && (
             <>
               <script
@@ -19,19 +20,46 @@ export default class MyDocument extends Document {
               <script
                 dangerouslySetInnerHTML={{
                   __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
                 }}
               />
             </>
           )}
+          {/* Enables Google Tag Manager only in production. */}
+          {isProduction && GTM_TRACKING_ID && (
+            <>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');`,
+                }}
+              />
+            </>
+          )}
+          {/* End Google Tag Manager */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          {isProduction && GTM_TRACKING_ID && (
+            <>
+              <noscript
+                dangerouslySetInnerHTML={{
+                  __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_TRACKING_ID}" height="0" width="0" 
+                  style="display:none;visibility:hidden"></iframe>`,
+                }}
+              />
+            </>
+          )}
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
