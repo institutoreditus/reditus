38b7428c86da11f2a484a75d771a27b0ac2084d8
Jonatas Teixeira <jonatasteixeira@id.uff.br>
2021-02-13 17:40:27 -0300

    fix: change linear progress bar to primary color

--- components/InputDonationValues.tsx
+++ components/InputDonationValues.tsx
@@ -6,11 +6,20 @@ import { Checkbox } from "@rmwc/checkbox";
 import axios from "axios";
 import { useState } from "react";
 import { FormControl, FormHelperText, LinearProgress } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 import styles from "./Form.module.css";
 import RoxContainer from "../services/rox/RoxContainer";
 import service from "../services/rox/RoxService";
 import Link from "next/link";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00d4ff",
+    },
+  },
+});
 
 service(RoxContainer);
 
@@ -122,154 +131,156 @@ export const InputDonationValues = (props: any) => {
   }
 
   return (
-    <div>
-      {props.form.firstname && <h3>Hey {props.form.teste}!</h3>}
-      <NavigationButtons step={2} {...props} previousStep={validate} />
-      <div className={styles.donationValues}>
-        <div className={styles.defaultValues}>
-          <input
-            className={styles.defaultValues__value}
-            type="radio"
-            value={val1}
-            name="amountInCents"
-            disabled={loading}
-            onChange={update}
-            id="firstDefaultValue"
-          />
-          <label
-            className={styles.defaultValues__value}
-            htmlFor="firstDefaultValue"
-          >
-            R$ {val1}
-          </label>
-
-          <input
-            className={styles.defaultValues__value}
-            type="radio"
-            value={val2}
-            name="amountInCents"
-            disabled={loading}
-            onChange={update}
-            id="secondDefaultValue"
-          />
-          <label
-            className={styles.defaultValues__value}
-            htmlFor="secondDefaultValue"
-          >
-            R$ {val2}
-          </label>
-
-          <input
-            className={styles.defaultValues__value}
-            type="radio"
-            value={val3}
-            name="amountInCents"
-            disabled={loading}
-            onChange={update}
-            id="thirdDefaultValue"
-          />
-          <label
-            className={styles.defaultValues__value}
-            htmlFor="thirdDefaultValue"
-          >
-            R$ {val3}
-          </label>
-        </div>
-
-        <div id={styles.customValue}>
-          <FormControl error={errorInputValue} fullWidth={true}>
-            <NumberFormat
-              label="Quero doar outro valor..."
-              prefix={"R$"}
-              id={styles.customValue__input}
+    <ThemeProvider theme={theme}>
+      <div>
+        {props.form.firstname && <h3>Hey {props.form.teste}!</h3>}
+        <NavigationButtons step={2} {...props} previousStep={validate} />
+        <div className={styles.donationValues}>
+          <div className={styles.defaultValues}>
+            <input
+              className={styles.defaultValues__value}
+              type="radio"
+              value={val1}
               name="amountInCents"
-              customInput={TextField}
-              thousandSeparator={true}
-              allowNegative={false}
-              onValueChange={(values) => {
-                setErrorInputValue(false);
-                const value = values.value;
-                if (!value) return;
-
-                if (checkedRadio) {
-                  checkedRadio.checked = false;
-                }
-
-                props.update("amountInCents", value);
-                setUserInputValue(value);
-              }}
-              value={userInputValue}
-              fullwidth
-            />
-            {errorInputValue && (
-              <FormHelperText
-                id="input-value-component-error-text"
-                style={{ margin: 0 }}
-              >
-                Por favor, selecione ou forneça um valor para doação de, no
-                minimo, 5 reais.
-              </FormHelperText>
-            )}
-          </FormControl>
-        </div>
-
-        <p>Vou doar: R$ {props.form.amountInCents}</p>
-        <div style={{ display: "inline-block" }}>
-          <FormControl error={errorConsent} fullWidth={true}>
-            <Checkbox
-              className={styles.checkbox}
               disabled={loading}
-              label={
-                <div>
-                  Li e aceito os{" "}
-                  <Link href={"/terms"} passHref>
-                    <a target="_blank" style={{ color: "#00d4ff" }}>
-                      Termos de Uso
-                    </a>
-                  </Link>{" "}
-                  e{" "}
-                  <Link href={"/privacy"}>
-                    <a target="_blank" style={{ color: "#00d4ff" }}>
-                      Politica de Privacidade
-                    </a>
-                  </Link>
-                </div>
-              }
-              type="checkbox"
-              name="consentCheckbox"
-              onChange={(e: any) => {
-                setPrivacyTermsAck(e.target.checked);
-                setErrorConsent(false);
-              }}
+              onChange={update}
+              id="firstDefaultValue"
             />
-            {errorConsent && (
-              <FormHelperText
-                id="terms-privacy-component-error-text"
-                style={{ margin: 0 }}
-              >
-                Por favor, leia nossos termos de uso antes de prosseguir.
-              </FormHelperText>
-            )}
-          </FormControl>
+            <label
+              className={styles.defaultValues__value}
+              htmlFor="firstDefaultValue"
+            >
+              R$ {val1}
+            </label>
+
+            <input
+              className={styles.defaultValues__value}
+              type="radio"
+              value={val2}
+              name="amountInCents"
+              disabled={loading}
+              onChange={update}
+              id="secondDefaultValue"
+            />
+            <label
+              className={styles.defaultValues__value}
+              htmlFor="secondDefaultValue"
+            >
+              R$ {val2}
+            </label>
+
+            <input
+              className={styles.defaultValues__value}
+              type="radio"
+              value={val3}
+              name="amountInCents"
+              disabled={loading}
+              onChange={update}
+              id="thirdDefaultValue"
+            />
+            <label
+              className={styles.defaultValues__value}
+              htmlFor="thirdDefaultValue"
+            >
+              R$ {val3}
+            </label>
+          </div>
+
+          <div id={styles.customValue}>
+            <FormControl error={errorInputValue} fullWidth={true}>
+              <NumberFormat
+                label="Quero doar outro valor..."
+                prefix={"R$"}
+                id={styles.customValue__input}
+                name="amountInCents"
+                customInput={TextField}
+                thousandSeparator={true}
+                allowNegative={false}
+                onValueChange={(values) => {
+                  setErrorInputValue(false);
+                  const value = values.value;
+                  if (!value) return;
+
+                  if (checkedRadio) {
+                    checkedRadio.checked = false;
+                  }
+
+                  props.update("amountInCents", value);
+                  setUserInputValue(value);
+                }}
+                value={userInputValue}
+                fullwidth
+              />
+              {errorInputValue && (
+                <FormHelperText
+                  id="input-value-component-error-text"
+                  style={{ margin: 0 }}
+                >
+                  Por favor, selecione ou forneça um valor para doação de, no
+                  minimo, 5 reais.
+                </FormHelperText>
+              )}
+            </FormControl>
+          </div>
+
+          <p>Vou doar: R$ {props.form.amountInCents}</p>
+          <div style={{ display: "inline-block" }}>
+            <FormControl error={errorConsent} fullWidth={true}>
+              <Checkbox
+                className={styles.checkbox}
+                disabled={loading}
+                label={
+                  <div>
+                    Li e aceito os{" "}
+                    <Link href={"/terms"} passHref>
+                      <a target="_blank" style={{ color: "#00d4ff" }}>
+                        Termos de Uso
+                      </a>
+                    </Link>{" "}
+                    e{" "}
+                    <Link href={"/privacy"}>
+                      <a target="_blank" style={{ color: "#00d4ff" }}>
+                        Politica de Privacidade
+                      </a>
+                    </Link>
+                  </div>
+                }
+                type="checkbox"
+                name="consentCheckbox"
+                onChange={(e: any) => {
+                  setPrivacyTermsAck(e.target.checked);
+                  setErrorConsent(false);
+                }}
+              />
+              {errorConsent && (
+                <FormHelperText
+                  id="terms-privacy-component-error-text"
+                  style={{ margin: 0 }}
+                >
+                  Por favor, leia nossos termos de uso antes de prosseguir.
+                </FormHelperText>
+              )}
+            </FormControl>
+          </div>
         </div>
+        <Button
+          label="Doar agora"
+          raised
+          unelevated
+          disabled={loading}
+          onClick={onCheckout}
+          id={styles.defaultButton}
+        />
+        {loading && <LinearProgress color="primary" />}
+        <br />
+        <br />
+        Quer nos ajudar doando ainda mais? Envie um email para{" "}
+        <a href="mailto:contato@reditus.org.br" style={{ color: "#00d4ff" }}>
+          <span color="#00d4ff">contato@reditus.org.br</span>
+        </a>{" "}
+        e conversamos em mais detalhes!
       </div>
-      <Button
-        label="Doar agora"
-        raised
-        unelevated
-        disabled={loading}
-        onClick={onCheckout}
-        id={styles.defaultButton}
-      />
-      {loading && <LinearProgress />}
-      <br />
-      <br />
-      Quer nos ajudar doando ainda mais? Envie um email para{" "}
-      <a href="mailto:contato@reditus.org.br" style={{ color: "#00d4ff" }}>
-        <span color="#00d4ff">contato@reditus.org.br</span>
-      </a>{" "}
-      e conversamos em mais detalhes!
-    </div>
+    </ThemeProvider>
   );
 };
 
