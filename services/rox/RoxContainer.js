bece6b0fca5b910757aca3dd06d58f6167f67b5b
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2021-02-13 11:10:17 -0500

    fix: have the app not depend on feature flag keys
    Signed-off-by: Jonatas Teixeira <jonatasteixeira@id.uff.br>

--- services/rox/RoxContainer.js
+++ services/rox/RoxContainer.js
@@ -1,12 +1,31 @@
 import { Rox, Flag, Variant } from "rox-ssr";
+
+class FakeFlag {
+  constructor(value) {
+    this.value = value;
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
 if (!Rox.containerCache) {
-  Rox.containerCache = {
-    suggestedDonationValues: new Variant("25|75|150", [
-      "25|75|150",
-      "50|150|300",
-      "150|300|500",
-    ]),
-    shouldShowRegistrationForm: new Flag(),
-  };
+  const key = process.env.ROLLOUT_API_KEY || "";
+  if (!key) {
+    Rox.containerCache = {
+      suggestedDonationValues: new FakeFlag("25|75|100"),
+      shouldShowRegistrationForm: new FakeFlag(false),
+    };
+  } else {
+    Rox.containerCache = {
+      suggestedDonationValues: new Variant("25|75|150", [
+        "25|75|150",
+        "50|150|300",
+        "150|300|500",
+      ]),
+      shouldShowRegistrationForm: new Flag(),
+    };
+  }
 }
 export default Rox.containerCache;
