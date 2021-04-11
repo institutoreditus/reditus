d94a08a6dedbf64f94fe9a2d92168938c4f23604
Rafael Rozendo <rafael.g.rozendo@gmail.com>
2021-04-11 00:12:51 -0300

    feat: API to return the balance
    Must implement tests

---
+++ pages/api/balance/index.ts
@@ -0,0 +1,89 @@
+import runRequestWithDIContainer from "../../../middlewares/diContainerMiddleware";
+import { DIContainerNextApiRequest } from "../../../dependency_injection/DIContainerNextApiRequest";
+import { NextApiRequest, NextApiResponse } from "next";
+import convertQueryParamToDate from "../../../helpers/datehelper";
+import getBalance, { BalanceGrouping } from "../../../use_cases/getBalance";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    await runRequestWithDIContainer(req, res, runGetBalance);
+  } else {
+    res.statusCode = 405;
+    res.send("");
+  }
+};
+
+async function runGetBalance(
+  req: DIContainerNextApiRequest,
+  res: NextApiResponse
+) {
+  let fromDate: Date | undefined = undefined;
+  let toDate: Date | undefined = undefined;
+  let groupBy: BalanceGrouping | undefined = undefined;
+
+  // validate dates
+  const fromDateFromQuery: string | string[] = req.query["from"];
+  const toDateFromQuery: string | string[] = req.query["to"];
+
+  try {
+    if (fromDateFromQuery) {
+      if (Array.isArray(fromDateFromQuery)) {
+        res.statusCode = 400;
+        res.send('Parametro "from" informado mais de 1 vez');
+        return;
+      }
+      fromDate = convertQueryParamToDate(fromDateFromQuery);
+    }
+    if (toDateFromQuery) {
+      if (Array.isArray(toDateFromQuery)) {
+        res.statusCode = 400;
+        res.send('Parametro "to" informado mais de 1 vez');
+        return;
+      }
+      toDate = convertQueryParamToDate(toDateFromQuery);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      res.statusCode = 422;
+      res.send(err.message);
+    } else {
+      res.statusCode = 500;
+      console.log(err);
+      res.send("");
+    }
+    return;
+  }
+
+  if (fromDate && toDate && fromDate > toDate) {
+    res.statusCode = 422;
+    res.send("A data inicial deve ser menor que a data final");
+    return;
+  }
+
+  // validate groupBy
+  const groupByFromQuery: string | string[] = req.query["groupBy"];
+  if (groupByFromQuery) {
+    if (Array.isArray(groupByFromQuery)) {
+      res.statusCode = 400;
+      res.send('Parametro "groupBy" informado mais de 1 vez');
+      return;
+    }
+
+    if (groupByFromQuery in BalanceGrouping) {
+      groupBy = BalanceGrouping[groupByFromQuery];
+    } else {
+      res.statusCode = 422;
+      res.send("O agrupamento deve ser por: week, month, year");
+      return;
+    }
+  }
+
+  const balance = await getBalance({
+    dbClient: req.scope.resolve("dbClient"),
+    fromDate: fromDate,
+    toDate: toDate,
+    groupBy: groupBy,
+  });
+
+  (res.statusCode = 200), res.json(balance);
+}
---
+++ use_cases/getBalance.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+
+export enum BalanceGrouping {
+  week = "week",
+  month = "month",
+  year = "year",
+}
+
+interface CreateContributionArgs {
+  dbClient: PrismaClient;
+  fromDate?: Date;
+  toDate?: Date;
+  groupBy?: BalanceGrouping;
+}
+
+export default async function getBalance(args: CreateContributionArgs) {
+  const fromDateCondition = args.fromDate ?? new Date("0000-01-01");
+  const toDateCondition = args.toDate ?? new Date("9999-12-31");
+
+  if (args.groupBy) {
+    return await getBalanceWithGrouping(
+      args.groupBy,
+      fromDateCondition,
+      toDateCondition,
+      args.dbClient
+    );
+  } else {
+    return await getBalanceWithoutGrouping(
+      fromDateCondition,
+      toDateCondition,
+      args.dbClient
+    );
+  }
+}
+
+async function getBalanceWithoutGrouping(
+  fromDate: Date,
+  toDate: Date,
+  dbClient: PrismaClient
+) {
+  const sum = await dbClient.contribution.aggregate({
+    sum: {
+      amountInCents: true,
+    },
+    where: {
+      AND: [
+        {
+          createdAt: {
+            gte: fromDate,
+          },
+        },
+        {
+          createdAt: {
+            lte: toDate,
+          },
+        },
+      ],
+    },
+  });
+  const response = [
+    {
+      referenceDate: new Date(),
+      balance: sum["sum"]["amountInCents"],
+    },
+  ];
+
+  return response;
+}
+
+async function getBalanceWithGrouping(
+  groupBy: BalanceGrouping,
+  fromDate: Date,
+  toDate: Date,
+  dbClient: PrismaClient
+) {
+  // TODO(rrozendo): I couldn't find how to do a "sum over" with prisma, and I wouldn't like to do the aggregation in memory. That's why I am using a raw query
+  const query: string =
+    'select distinct date_trunc($1,"createdAt") as "referenceDate",  sum(amount_in_cents) over(order by date_trunc($2,"createdAt")) as "balance" from contributions where "createdAt" >= $3 and "createdAt" <= $4 order by 1;';
+  const result: Array<any> = await dbClient.$queryRaw(
+    query,
+    BalanceGrouping[groupBy],
+    BalanceGrouping[groupBy],
+    fromDate,
+    toDate
+  );
+
+  // convert to date to keep the same format as the getBalanceWithoutGrouping method
+  result.forEach((element: any) => {
+    element["referenceDate"] = new Date(element["referenceDate"]);
+  });
+
+  return result;
+}
