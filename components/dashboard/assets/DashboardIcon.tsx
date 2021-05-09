a71cbcf7144a59870663072207e37d7c510552c7
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2021-05-09 16:13:17 -0300

    fix: ajust svg icon

--- components/dashboard/assets/DashboardIcon.tsx
+++ components/dashboard/assets/DashboardIcon.tsx
@@ -3,37 +3,13 @@ import SvgIcon from "@material-ui/core/SvgIcon";
 export const DashboardIcon = (props: any) => {
   return (
     <SvgIcon className={props.className}>
-      <svg
-        width="24"
-        height="20"
-        viewBox="0 0 14 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          y="0.979492"
-          width="7.95721"
-          height="8.03626"
-          fill={props.color}
-        />
-        <rect
-          x="10.042"
-          y="0.979492"
-          width="7.95721"
-          height="8.03626"
-          fill={props.color}
-        />
-        <rect
-          x="10.042"
-          y="11.123"
-          width="7.95721"
-          height="8.03626"
-          fill={props.color}
-        />
-        <rect y="11.123" width="7.95721" height="8.03626" fill={props.color} />
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.0728 0H0V7.07294H7.0728V0ZM0 8.92706V16H7.0728V8.92706H0ZM8.9272 16H16V8.92706H8.9272V16ZM16 7.07294V0H8.9272V7.07294H16Z" fill="#00D4FF"/>
       </svg>
     </SvgIcon>
   );
 };
 
 export default DashboardIcon;
+
+
