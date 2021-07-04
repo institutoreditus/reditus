47f11350d953026ee173a906bf38936206626838
Rafael Ferreira <rafaelcardoso@poli.ufrj.br>
2021-07-03 21:02:00 -0400

    Add callout for the donation campaign

--- components/SelectDonationMode.tsx
+++ components/SelectDonationMode.tsx
@@ -6,11 +6,25 @@ const SelectDonationMode = (props: any) => {
   return (
     <div className={styles.donationModeWrapper}>
       <p className="title">
-        Faça parte dessa corrente do bem! Ajude a fomentar uma cultura de
-        retribuição.
+        <b>
+          Todas as doações feitas no mês de Julho serão <u>triplicadas</u>!
+        </b>
+        *
       </p>
+
       <SubscriptionButton step={1} {...props} />
       <ContributionButton step={1} {...props} />
+      <p>
+        <span style={{ fontSize: "small" }}>
+          * Válido de 04 a 31 de Julho de 2021
+        </span>
+      </p>
+      <p>
+        <span style={{ fontSize: "small" }}>
+          * Para doações recorrentes, apenas a parcela referente ao mês de Julho
+          de 2021 será triplicada
+        </span>
+      </p>
     </div>
   );
 };
