d47d7ba624cdb65b051a60b54594cdb0144f0e8f
Jonatas Teixeira <32902628+JonatasT@users.noreply.github.com>
2021-07-19 20:55:46 -0300

    fix: add condition (#86)

--- components/SelectDonationMode.tsx
+++ components/SelectDonationMode.tsx
@@ -16,13 +16,18 @@ const SelectDonationMode = (props: any) => {
       <ContributionButton step={1} {...props} />
       <p>
         <span style={{ fontSize: "small" }}>
-          * Válido de 04 a 31 de Julho de 2021
+          * Válido para novas doações realizadas de 04 a 31 de Julho de 2021
         </span>
       </p>
       <p>
         <span style={{ fontSize: "small" }}>
-          * Para doações recorrentes, apenas a parcela referente ao mês de Julho
-          de 2021 será triplicada
+          * Para novas doações recorrentes, apenas a parcela referente ao mês de
+          Julho de 2021 será triplicada
+        </span>
+      </p>
+      <p>
+        <span style={{ fontSize: "small" }}>
+          * Limitado a R$ 5.000 por CPF.
         </span>
       </p>
       <p>
