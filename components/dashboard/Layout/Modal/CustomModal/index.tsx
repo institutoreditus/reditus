9435ce514ddb6bc2845831fb5b623f12d5672ea2
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2021-07-31 19:38:23 -0300

    feat: custom modal

---
+++ components/dashboard/Layout/Modal/CustomModal/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  })
+);
+
+export const CustomModal = (props: any, children: any) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={props.openModal}
+        onClose={props.handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={props.openModal}>{children}</Fade>
+      </Modal>
+    </>
+  );
+};
+
+export default CustomModal;
