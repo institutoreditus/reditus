a6acc2db76b683cf9bac6608b5b8a14357bf76c4
Jonatas da Silva Teixeira <jonatasteixeira@id.uff.br>
2021-07-31 18:17:17 -0300

    fix: change rmwc to material-ui

--- components/checkout_buttons/SubscriptionButton.tsx
+++ components/checkout_buttons/SubscriptionButton.tsx
@@ -1,6 +1,27 @@
-import { Button } from "@rmwc/button";
-import styles from "../Form.module.css";
+import Button from "@material-ui/core/Button";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
 import { ReditusEvent, push } from "../../helpers/gtm";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+    outlinedButton: {
+      color: "#00d4ff",
+      width: "100%",
+      height: "100%",
+      padding: "1.5rem",
+      border: "2px solid #00d4ff",
+      borderRadius: "2px",
+      textTransform: "none",
+      fontSize: "15px",
+    },
+  })
+);
 
 export const SubscriptionButton = ({
   nextStep,
@@ -8,6 +29,7 @@ export const SubscriptionButton = ({
   step,
   update,
 }: any) => {
+  const classes = useStyles();
   const setDonationModeAndGoToNextStep = (e: any) => {
     // Pushing to data layer is used by the GTM.
     push(ReditusEvent.click, "Donate monthly");
@@ -19,15 +41,16 @@ export const SubscriptionButton = ({
     <div>
       {step < totalSteps && (
         <Button
-          label="Doar mensalmente"
           name="donationMode"
           id="donateMonthly"
-          className={styles.outlinedButton}
-          icon="done_all"
+          variant="outlined"
+          startIcon={<DoneAllIcon />}
+          className={classes.outlinedButton}
           value="subscriptions"
           onClick={setDonationModeAndGoToNextStep}
-          outlined
-        />
+        >
+          Doar mensalmente
+        </Button>
       )}
     </div>
   );
