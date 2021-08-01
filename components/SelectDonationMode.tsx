1d1fe1061eef30a60cd12b886cbca8822688ede1
Rafael Cardoso <rafaelcardoso@poli.ufrj.br>
2021-08-01 10:33:37 -0400

    feat: end donation campaign

--- components/SelectDonationMode.tsx
+++ components/SelectDonationMode.tsx
@@ -6,30 +6,11 @@ const SelectDonationMode = (props: any) => {
   return (
     <div className={styles.donationModeWrapper}>
       <p className="title">
-        <b>
-          Todas as doações feitas no mês de Julho serão <u>triplicadas</u>!
-        </b>
-        *
+        Faça parte dessa corrente do bem! Ajude a fomentar uma cultura de
+        retribuição.
       </p>
-
       <SubscriptionButton step={1} {...props} />
       <ContributionButton step={1} {...props} />
-      <p>
-        <span style={{ fontSize: "small" }}>
-          * Válido para novas doações realizadas de 04 a 31 de Julho de 2021
-        </span>
-      </p>
-      <p>
-        <span style={{ fontSize: "small" }}>
-          * Para novas doações recorrentes, apenas a parcela referente ao mês de
-          Julho de 2021 será triplicada
-        </span>
-      </p>
-      <p>
-        <span style={{ fontSize: "small" }}>
-          * Limitado a R$ 5.000 por CPF.
-        </span>
-      </p>
     </div>
   );
 };
