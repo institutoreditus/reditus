92b0ab598d5b7dc14768001bade7854714bab1fe
Rafael Cardoso <rafaelcardoso@poli.ufrj.br>
2021-09-23 18:40:58 -0400

    Add facebook pixel config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,6 +46,32 @@ export default class MyDocument extends Document {
             </>
           )}
           {/* End Google Tag Manager */}
+          {/* Enabled Facebook Pixel only in production. */}
+          {isProduction && (
+            <>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  <script>
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '3225839960976651');
+                  fbq('track', 'PageView');
+                  </script>
+                  <noscript><img height="1" width="1" style="display:none"
+                  src="https://www.facebook.com/tr?id=3225839960976651&ev=PageView&noscript=1"
+                  /></noscript>`,
+                }}
+              />
+            </>
+          )}
+          {/* End Facebook Pixel */}
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
