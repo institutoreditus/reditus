4463a04931b2d467b3b0f110e1fdda366a194698
Estevam Furtado <estevamfurtado@gmail.com>
2022-11-14 17:54:42 -0300

    feat: facebook pixel (#114)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,6 +49,19 @@ function App({ Component, pageProps }: AppProps) {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("vf4ad1m6a1m5w98ttfp3j2ilt557on");
+        ReactPixel.pageView();
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
